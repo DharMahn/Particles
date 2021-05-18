c95d42f345f4c530a8d04758b21471e7cf2ea4fa
DharMahn <felnagy.zoltan@gmail.com>
2021-05-18 10:11:30 +0200

    initial commit

---
+++ monogame particles/Game1.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace monogame_particles
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        const int max = 10;
+        Particle[] particles = new Particle[max];
+        System.Drawing.Rectangle bounds;
+        Texture2D pixel;
+        
+        Random r = new Random();
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            //bounds = System.Windows.Forms.Screen.AllScreens.Select(screen => screen.Bounds).Aggregate(System.Drawing.Rectangle.Union);
+            bounds = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
+            graphics.PreferredBackBufferWidth = bounds.Width;
+            graphics.PreferredBackBufferHeight = bounds.Height;
+            Window.Position = new Point(0, 0);
+            graphics.SynchronizeWithVerticalRetrace = false;
+            IsFixedTimeStep = false;
+            TargetElapsedTime = TimeSpan.FromSeconds(1 / 144.0f);
+
+            for (int i = 0; i < max; i++)
+            {
+                particles[i] = new Particle(new Vector2(r.Next(bounds.Width), r.Next(bounds.Height)), new Color(r.Next(256), r.Next(256), r.Next(256)), bounds.Width, bounds.Height,r.Next(2));
+            }
+            Window.IsBorderless = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            pixel = new Texture2D(GraphicsDevice, 1, 1);
+            pixel.SetData<Color>(new Color[1] { Color.White });
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            MouseState ms = Mouse.GetState();
+            Parallel.For(0, max, i =>
+            {
+                particles[i].Update(new Vector2(ms.X, ms.Y), ms.LeftButton == ButtonState.Pressed,particles);
+            });
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            /*GraphicsDevice.Textures[0] = null;
+            pixels = new uint[bounds.Width * bounds.Height];
+            Parallel.For(0, max, i =>
+            {
+                int index = ((bounds.Width * (int)particles[i].position.Y) + (int)particles[i].position.X);
+                pixels[index] = ToUint(particles[i].color);
+            });
+            canvas.SetData(pixels, 0, bounds.Width * bounds.Height);*/
+            spriteBatch.Begin();
+            for (int i = 0; i < max; i++)
+            {
+                spriteBatch.Draw(pixel, new Rectangle((int)particles[i].position.X, (int)particles[i].position.Y, 1, 1), particles[i].color);
+            }
+            /*Parallel.For(0, max, i =>
+            {
+                
+            });*/
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+        private uint ToUint(Color c)
+        {
+            return (uint)(((c.A << 24) | (c.R << 16) | (c.G << 8) | c.B) & 0xffffffffL);
+        }
+    }
+}
---
+++ monogame particles/Particle.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace monogame_particles
+{
+    class Particle
+    {
+        public Vector2 position;
+        public Vector2 velocity;
+        int maxX;
+        int maxY;
+        public Color color;
+        int boundWidth = 0;
+        public int group;
+        public Particle(Vector2 position, Color color, int maxX, int maxY, int group)
+        {
+            this.position = position;
+            this.color = color;
+            this.maxX = maxX;
+            this.maxY = maxY;
+            this.group = group;
+        }
+        public Particle(Color color, int maxX, int maxY)
+        {
+            position = new Vector2(10, 10);
+            this.color = color;
+            this.maxX = maxX;
+            this.maxY = maxY;
+        }
+        public void Update(Vector2 cursorPos, bool mouseDown, Particle[] particles)
+        {
+            Console.WriteLine(position.ToString());
+            //Attract(cursorPos, mouseDown);
+            foreach(Particle p in particles)
+            {
+                if(p.group == group)
+                {
+                    velocity += Attract(p.position);
+                }
+            }
+            velocity *= .975f;
+            position += velocity;
+            if (position.X < boundWidth)
+            {
+                position.X = boundWidth;
+                velocity.X *= -1;
+            }
+            else if (position.X >= maxX - boundWidth)
+            {
+                position.X = maxX - boundWidth - 1;
+                velocity.X *= -1;
+            }
+            if (position.Y < boundWidth)
+            {
+                position.Y = boundWidth;
+                velocity.Y *= -1;
+            }
+            else if (position.Y >= maxY - boundWidth)
+            {
+                position.Y = maxY - boundWidth - 1;
+                velocity.Y *= -1;
+            }
+        }
+
+        Vector2 Attract(Vector2 pos)
+        {
+            Vector2 d = position - pos;
+            var angle = Math.Atan2(d.Y, d.X);
+            Vector2 acceleration = new Vector2();
+            /*acceleration.X = (1 / d.Length()) * (float)Math.Cos(angle);
+            acceleration.Y = (1 / d.Length()) * (float)Math.Sin(angle);*/
+            acceleration = d;
+            acceleration.Normalize();
+            return acceleration;
+        }
+
+        Vector2 AttractCursor(Vector2 cursorPos, bool mouseDown)
+        {
+            Vector2 d = position - cursorPos;
+            var angle = Math.Atan2(d.Y, d.X);
+            Vector2 acceleration = new Vector2();
+            if (d.Length() > 10 && d.Length() < 300)
+            {
+                if (!mouseDown)
+                {
+                    acceleration.X = (20 / d.Length()) * (float)Math.Cos(angle);
+                    acceleration.Y = (20 / d.Length()) * (float)Math.Sin(angle);
+                    return acceleration;
+                }
+                else
+                {
+                    acceleration.X = (250 / d.Length()) * (float)Math.Cos(angle);
+                    acceleration.Y = (250 / d.Length()) * (float)Math.Sin(angle);
+                    return acceleration;
+                }
+            }
+            return acceleration;
+        }
+    }
+}
